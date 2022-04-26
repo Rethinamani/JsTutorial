f28b394d876251d99951808752b942d2bc8b2bfd
Rethinamani <kaviyamohan65@gmail.com>
2022-04-26 20:58:33 +0530

    try

--- JsExamples/Ex_17/ex_17.js
+++ JsExamples/Ex_17/ex_17.js
@@ -1,10 +1,11 @@
 let a = 1;
 for(let i = 1; i<= 3; i++){
     for(let j=1;j<= 3-i;j++){
+        document.write("\n");
         document.write("&nbsp;");
     }
-    for(let k = 1; k<= i; k++){
-        document.write("* ");
+    for(let k = 1; k<= a; k++){
+        document.write("*");
     }
     a += 2;
     document.write("<br>");
