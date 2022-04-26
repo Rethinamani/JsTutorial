1f8d5f362ffe3dc80b7759d30332482d210e6353
Rethinamani <kaviyamohan65@gmail.com>
2022-04-26 20:53:11 +0530

    new

---
+++ JsExamples/Ex_17/ex_17.js
@@ -0,0 +1,11 @@
+let a = 1;
+for(let i = 1; i<= 3; i++){
+    for(let j=1;j<= 3-i;j++){
+        document.write("&nbsp;");
+    }
+    for(let k = 1; k<= i; k++){
+        document.write("* ");
+    }
+    a += 2;
+    document.write("<br>");
+}
